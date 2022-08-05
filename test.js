f1042edacf43556470368c3ec96670c67cf4683e
Matth-s <108790362+Matth-s@users.noreply.github.com>
2022-08-05 10:41:30 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,35 @@
 const button = document.querySelector('.icone-menu-deroulant');
 const icone = document.querySelector('#menu');
+const menuDeroulant = document.querySelectorAll('.menu-deroulant');
+const array = document.querySelectorAll('.sous-menu');
 
 button.addEventListener('click', function() {
     icone.classList.toggle('icone-menu-deroulant-js');
-    console.log("hamburger")
+    array.forEach((items) => {
+        items.classList.remove('display');
+    });
+});
+
+
+
+menuDeroulant.forEach((itemDeroulant) => {
+    itemDeroulant.addEventListener('click', function(e) {
+        switch(e.target.id) {
+            case "un":
+                array[0].classList.toggle('display');
+                break;
+            case "deux":
+                array[1].classList.toggle('display');
+                break;
+            case "trois":
+                array[2].classList.toggle('display');
+                break;
+            case "quatre":
+                array[3].classList.toggle('display');
+                break;
+            case "cinq":
+                array[4].classList.toggle('display');
+                break;
+        }
+        })
 });
